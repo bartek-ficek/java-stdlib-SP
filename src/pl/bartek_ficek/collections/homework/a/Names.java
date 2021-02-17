c59235ea61f49a1f4319bfcde9a2cc2358059e32
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-17 22:36:28 +0100

    1.Collections - Homework 1 -continuation of  implementation method addToList()

--- src/pl/bartek_ficek/collections/homework/a/Names.java
+++ src/pl/bartek_ficek/collections/homework/a/Names.java
@@ -1,18 +1,20 @@
 package pl.bartek_ficek.collections.homework.a;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Names {
 
-    Scanner scanner = new Scanner(System.in);
-    private List<String> names;
+    private final Scanner scanner = new Scanner(System.in);
+    private List<String> names = new ArrayList<>();
 
     public void addToList() {
         do {
-            System.out.println("Type name and hit the Enter to add it to list. If you want to break adding type \"-\".\n");
+            System.out.println("\nType name and hit the Enter to add it to list. If you want to break adding type \"-\".\n");
             String name = scanner.next();
             if (name.equals("-")) {
+                System.out.println(Arrays.toString(names.toArray()));
+                Set<String> uniqueNames = new HashSet<>(names);
+                System.out.println("You have " + uniqueNames.size() + " on your list.");
                 break;
             }
             names.add(name);
